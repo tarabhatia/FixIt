e85b3b460e0ab514ae24bd250c493a9a75e66c8d
Tara Bhatia <tarabhatia@fb.com>
2019-07-16 14:00:19 -0700

    initial server setup

--- app/src/main/java/com/example/fixit/MainActivity.java
+++ app/src/main/java/com/example/fixit/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.fixit;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView test;
+    Button button;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference conditionRef = mRootRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        test = findViewById(R.id.test);
+        button = findViewById(R.id.button);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        conditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                test.setText(text);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                conditionRef.setValue("Hello World");
+            }
+        });
+
+
+
+
     }
 }
