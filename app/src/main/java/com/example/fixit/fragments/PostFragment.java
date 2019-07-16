dab2ff5d519404e126c7030860388e209dfc120f
Tara Bhatia <tarabhatia@fb.com>
2019-07-16 14:50:54 -0700

    starting fragments and bottom navigation

---
+++ app/src/main/java/com/example/fixit/fragments/PostFragment.java
@@ -0,0 +1,39 @@
+package com.example.fixit.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.fixit.R;
+
+public class PostFragment extends Fragment {
+
+    Button btnTest;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnTest = view.findViewById(R.id.btnTest);
+
+        btnTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Button works!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
