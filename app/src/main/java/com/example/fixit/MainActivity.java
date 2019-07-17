435b2ee318a38b43d92a642233632b8ddbc707f5
Tara Bhatia <tarabhatia@fb.com>
2019-07-17 09:56:33 -0700

    fixed server and connected it to fragments

--- app/src/main/java/com/example/fixit/MainActivity.java
+++ app/src/main/java/com/example/fixit/MainActivity.java
@@ -1,16 +1,11 @@
 package com.example.fixit;
 
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -22,76 +17,47 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    //    private TextView test;
-//    private Button button;
     private FirebaseAuth mAuth;
     private EditText usernameSignUp;
     private EditText passwordSignUp;
     private Button signUpbtn;
-    TextView test;
-    Button button;
-    Button btnUserActivity;
 
-
-//    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-//    DatabaseReference conditionRef = mRootRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        test = findViewById(R.id.test);
-//        button = findViewById(R.id.button);
         usernameSignUp = findViewById(R.id.username_signin_et);
         passwordSignUp = findViewById(R.id.password_signin_et);
         signUpbtn = findViewById(R.id.btnSignUp);
-        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
 
+        // Initialize Firebase Auth
+        signUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("SignUp", "break");
+                SignUp();
+                Intent intent = new Intent(MainActivity.this, UserActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        // Initialize Firebase Auth
-        mAuth = FirebaseAuth.getInstance();
-        signUpbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SignUp();
-//                final Intent intent = new Intent(MainActivity.this, UserActivity.class);
-//                startActivity(intent);
-//                finish();
-            }
-        });
-//        conditionRef.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                String text = dataSnapshot.getValue(String.class);
-//                test.setText(text);
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-//
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                conditionRef.setValue("Hello World");
-//            }
-//        });
 
     }
 
     private void SignUp() {
         String email = usernameSignUp.getText().toString(), password = passwordSignUp.getText().toString();
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
@@ -105,5 +71,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
     }
 }
