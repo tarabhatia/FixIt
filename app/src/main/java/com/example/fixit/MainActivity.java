621ffff523b363d77e3cbbaaf817e61dc92f2367
Jose Alberto López <albertolm9115@fb.com>
2019-07-16 17:12:07 -0700

    Sign Up

--- app/src/main/java/com/example/fixit/MainActivity.java
+++ app/src/main/java/com/example/fixit/MainActivity.java
@@ -1,61 +1,93 @@
 package com.example.fixit;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView test;
-    Button button;
+//    private TextView test;
+//    private Button button;
+    private FirebaseAuth mAuth;
+    private EditText usernameSignUp;
+    private EditText passwordSignUp;
+    private Button signUpbtn;
 
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    DatabaseReference conditionRef = mRootRef.child("condition");
+
+//    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+//    DatabaseReference conditionRef = mRootRef.child("condition");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        test = findViewById(R.id.test);
-        button = findViewById(R.id.button);
-
+        setContentView(R.layout.activity_user);
+//        test = findViewById(R.id.test);
+//        button = findViewById(R.id.button);
+        usernameSignUp = findViewById(R.id.username_signin_et);
+        passwordSignUp = findViewById(R.id.password_signin_et);
+        signUpbtn = findViewById(R.id.btnSignUp);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        conditionRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String text = dataSnapshot.getValue(String.class);
-                test.setText(text);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-        button.setOnClickListener(new View.OnClickListener() {
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        signUpbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                conditionRef.setValue("Hello World");
+                SignUp();
             }
         });
+//        conditionRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                String text = dataSnapshot.getValue(String.class);
+//                test.setText(text);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                conditionRef.setValue("Hello World");
+//            }
+//        });
 
+    }
 
-
-
+    private void SignUp(){
+        String email = usernameSignUp.getText().toString(), password = passwordSignUp.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Log.d("SignUp", "createUserWithEmail:success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+                }
+                else{
+                    Log.w("SignUp", "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(MainActivity.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
