e1194d6fadc19c8590ea208d9c2a98a43a29a7c3
Tara Bhatia <tarabhatia@fb.com>
2019-07-18 12:32:29 -0700

    added marker and changed bottom nav color

--- app/src/main/java/com/example/fixit/fragments/MapFragment.java
+++ app/src/main/java/com/example/fixit/fragments/MapFragment.java
@@ -1,25 +1,42 @@
 package com.example.fixit.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.fixit.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     GoogleMap mGoogleMap;
     MapView mMapView;
     View mView;
+    FusedLocationProviderClient mFusedLocationProviderClient;
+    private static final float DEFAULT_ZOOM = 15f;
 
     public MapFragment() {
     }
@@ -31,8 +48,8 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-       mView = inflater.inflate(R.layout.fragment_map, container, false);
-       return mView;
+        mView = inflater.inflate(R.layout.fragment_map, container, false);
+        return mView;
     }
 
     @Override
@@ -54,6 +71,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
 
         MapsInitializer.initialize(getContext());
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng marker = new LatLng(37.452961, -122.181725);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(marker, DEFAULT_ZOOM));
+        googleMap.addMarker(new MarkerOptions().title("Testing marker").position(marker));
+
 
 
     }
